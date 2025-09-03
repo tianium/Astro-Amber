5d6f21307f8bff44914c1651ea661f3527dcb099
tianium <36811828+tianium@users.noreply.github.com>
2025-09-03 10:53:23 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tianium.github.io',
-  base: 'Astro-Amber',
+  base: '/Astro-Amber',
 });
